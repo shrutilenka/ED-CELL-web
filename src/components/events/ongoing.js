48930be02189dfc4f10ac43b5732bd96e1480f3d
shrutilenka <shrutilenka1202@gmail.com>
2023-02-03 22:16:33 +0530

    done dynamically handling cards

--- src/components/events/ongoing.js
+++ src/components/events/ongoing.js
@@ -1,10 +1,26 @@
 import { Card } from "antd";
 import "./../../styles/style.css";
+import OngoingR from "./../../ongoingEvents.json";
 
 const Ongoing = () => {
   return (
     <div className="cards">
-      <Card
+      {OngoingR.map((records) => {
+        return (
+          <Card
+            style={{
+              width: 400,
+              marginBottom: "4rem",
+            }}
+            cover={<img alt="example" src={records.src} />}
+            key={records.id}
+          >
+            <p>{records.title} </p>
+          </Card>
+        );
+      })}
+
+      {/* <Card
         style={{
           width: 400,
           marginBottom: "4rem",
@@ -87,7 +103,7 @@ const Ongoing = () => {
         }
       >
         <p>event 1 </p>
-      </Card>
+      </Card> */}
     </div>
   );
 };
