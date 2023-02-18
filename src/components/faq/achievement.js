3b34468a0eec0976e7d93cfc4c0b2de180cdbf3f
shrutilenka <shrutilenka1202@gmail.com>
2023-02-18 20:45:44 +0530

    embeded insta

---
+++ src/components/faq/achievement.js
@@ -0,0 +1,48 @@
+import { InstagramEmbed } from "react-social-media-embed";
+import { Carousel } from "antd";
+
+const contentStyle = {
+  height: "750px",
+  color: "#fff",
+  lineHeight: "160px",
+  textAlign: "center",
+  background: "#f4f4fc",
+};
+
+const Achieve = () => {
+  return (
+    // <div style={{ display: "flex", justifyContent: "center" }}>
+    //   <InstagramEmbed
+    //     url="https://www.instagram.com/p/CjZz8XkL4FU/?utm_source=ig_web_copy_link"
+    //     width={600}
+    //     height={800}
+    //   />
+    // </div>
+    <div>
+      <Carousel autoplay autoplaySpeed={2000} className="banner-carousel">
+        <div>
+          <div className="embed" style={contentStyle}>
+            <InstagramEmbed url="https://www.instagram.com/p/CjZz8XkL4FU/?utm_source=ig_web_copy_link" />
+          </div>
+        </div>
+        <div>
+          <div className="embed" style={contentStyle}>
+            <InstagramEmbed url="https://www.instagram.com/p/Cocrh5-L7Dh/?utm_source=ig_web_copy_link" />
+          </div>
+        </div>
+        <div>
+          <div className="embed" style={contentStyle}>
+            <InstagramEmbed url="https://www.instagram.com/p/CocrYqfrbyO/?utm_source=ig_web_copy_link" />
+          </div>
+        </div>
+        <div>
+          <div className="embed" style={contentStyle}>
+            <InstagramEmbed url="https://www.instagram.com/p/Clf4FnILpE4/?utm_source=ig_web_copy_link" />
+          </div>
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Achieve;
